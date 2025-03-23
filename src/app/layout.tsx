629cb3fb5ff75c3058d7de2657d73215d6e72690
jacam666 <ja6cam@gmail.com>
2025-03-23 20:20:43 +0000

    fixed font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
           rel="stylesheet"
