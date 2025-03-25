84d4deb58ae05792a3195e222ae2adee39612071
jacam666 <ja6cam@gmail.com>
2025-03-25 19:04:49 +0000

    added some bg styling

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -6,19 +6,30 @@ import Footer from '../components/footer';
 
 export default function Services() {
     return (
-        <div>
+
+        <div
+            className="min-h-screen bg-cover bg-center"
+            style={{
+                backgroundImage:
+                    "url('/images/light-brushed-metal-texture-silver-platinum-stock-wallpaper.jpg')",
+            }}
+            >
             <Navbar />
-            <div className='  bg-white'>
+            <div className=''>
                 <div>
                     <h1 className=' text-black text-4xl md:text-7xl text-center md:mx-8 p-10'>
                         Expert Window Installation Services
                     </h1>
-                    <h2 className='space-y-8 px-6 md:p-8 text-black text-center text-xl md:text-3xl font-medium md:mx-12 mb-4'>
+                    <h2 className='space-y-8 px-6 md:p-8 text-black text-center text-xl md:text-3xl font-medium md:mx-12 my-4'>
                         With 20 years experience in the trade you can be sure of an outstanding Design/Sale to project managed installation experience. With the added benefit of trade pricing
                     </h2>
                 </div>
-                <div className="flex flex-col">
-                    <div className="flex flex-col md:flex-row items-center p-2 bg-white">
+                <div className="flex flex-col" 
+                style={{
+                    backgroundImage:
+                        "url('/images/light-brushed-metal-texture-silver-platinum-stock-wallpaper.jpg')",
+                }}>
+                    <div className="flex flex-col md:flex-row items-center p-2 ">
                         <div className="w-full lg:w-1/2">
                             <Image
                                 src="/images/services1-image.avif"
@@ -39,9 +50,13 @@ export default function Services() {
 
                 <div className="h-1 bg-black my-4 w-3/4 mx-auto"></div>
 
-                <div className="flex flex-col">
-                    <div className="flex flex-col md:flex-row items-center p-2 bg-white">
-                    <div className="w-full lg:w-1/2">
+                <div className="flex flex-col" 
+                style={{
+                    backgroundImage:
+                        "url('/images/light-brushed-metal-texture-silver-platinum-stock-wallpaper.jpg')",
+                }}>
+                    <div className="flex flex-col md:flex-row items-center p-2">
+                        <div className="w-full lg:w-1/2">
                             <Image
                                 src="/images/services2-image.avif"
                                 height={400}
@@ -50,18 +65,21 @@ export default function Services() {
                                 priority
                             />
                         </div>
-                    <div className="w-full lg:w-1/2 flex flex-col font-medium text-center space-y-4 py-4">
-                            <h1 className="p-2 text-2xl lg:text-5xl text-black">Trade only</h1>
+                        <div className="w-full lg:w-1/2 flex flex-col font-medium text-center space-y-4 py-4 ">
+                            <h1 className="p-2 text-2xl lg:text-5xl text-black w-1/2 flex mx-auto">Trade only</h1>
                             <h1 className="p-2 text-2xl lg:text-2xl text-black">UPVC and Aluminium Windows</h1>
                         </div>
-                        
                     </div>
                 </div>
 
                 <div className="h-1 bg-black my-4 w-3/4 mx-auto"></div>
 
-                <div className="flex flex-col">
-                    <div className="flex flex-col md:flex-row items-center p-2 bg-white">
+                <div className="flex flex-col" 
+                style={{
+                    backgroundImage:
+                        "url('/images/light-brushed-metal-texture-silver-platinum-stock-wallpaper.jpg')",
+                }}>
+                    <div className="flex flex-col md:flex-row items-center p-2 ">
                         <div className="w-full lg:w-1/2">
                             <Image
                                 src="/images/services3-image.avif"
@@ -80,8 +98,8 @@ export default function Services() {
                     </div>
                 </div>
                 <h1 className="bg-cyan-950 text-white text-lg md:text-4xl text-center p-4 md:p-12">
-                Enhance the beauty and functionality of your home with our top-notch window installation services. Let us help you bring your vision to life with custom window solutions tailored to your needs.
-                    </h1>
+                    Enhance the beauty and functionality of your home with our top-notch window installation services. Let us help you bring your vision to life with custom window solutions tailored to your needs.
+                </h1>
             </div>
             <Footer />
         </div >
