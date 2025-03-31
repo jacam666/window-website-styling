3827cb57581f82e0f4928f81ac2442aac1fb6010
jacam666 <ja6cam@gmail.com>
2025-03-31 18:58:39 +0100

    added cards to remaining sections of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -35,63 +35,63 @@ export default function About() {
                         />
                     </div> */}
 
-                    <div className="w-full bg-white font-bold rounded-lg shadow-lg p-4 outline-1 ">
+                    <div className="w-full bg-white font-bold rounded-lg shadow-lg p-4 ">
                         <div className="grid sm:grid-cols-3 gap-4">
                             {/* Card 1 */}
                             <div className="bg-sky-900 rounded-lg shadow-md overflow-hidden">
-                                <h3 className="flex font-bold text-lg justify-center pt-8">Customers name</h3>
+                                <h3 className="flex font-bold text-xl justify-center pt-8">Customers name</h3>
                                 <div className="relative h-36">
                                     <Image
                                         src="/images/star-ratings (1).png"
                                         alt="Sunset Over the Sea"
                                         fill
-                                        className=""
+                                        className=" px-8"
                                     />
                                 </div>
-                                <div className="p-4">
-                                    <p className="text-white text-base">
-                                    &quot;Absolutely amazing service and quality. My new windows look fantastic, and the team was professional every step of the way.&quot;
+                                <div className="pb-4">
+                                    <p className="text-white text-base px-4 text-center">
+                                        &quot;Absolutely amazing service and quality. My new windows look fantastic, and the team was professional every step of the way.&quot;
                                     </p>
                                 </div>
                             </div>
                             {/* Card 2 */}
                             <div className="bg-sky-900 rounded-lg shadow-md overflow-hidden">
-                                <h3 className="flex font-bold text-lg justify-center pt-8">Customers name</h3>
+                                <h3 className="flex font-bold text-xl justify-center pt-8">Customers name</h3>
                                 <div className="relative h-36">
                                     <Image
                                         src="/images/star-ratings (1).png"
                                         alt="Sunset Over the Sea"
                                         fill
-                                        className=""
+                                        className="px-8"
                                     />
                                 </div>
-                                <div className="p-4">
-                                    <p className="text-white text-base">
+                                <div className="pb-4">
+                                    <p className="text-white text-base px-4 text-center">
                                         &quot;Efficient and friendly! The installation was prompt, and the trade pricing made it a no-brainer. Highly recommend Sightline Windows. &quot;
                                     </p>
                                 </div>
                             </div>
                             {/* Card 3 */}
                             <div className="bg-sky-900 rounded-lg shadow-md overflow-hidden">
-                                <h3 className="flex font-bold text-lg justify-center pt-8">Customers name</h3>
+                                <h3 className="flex font-bold text-xl justify-center pt-8">Customers name</h3>
                                 <div className="relative h-36">
                                     <Image
                                         src="/images/star-ratings (1).png"
                                         alt="Sunset Over the Sea"
                                         fill
-                                        className=""
+                                        className="px-8"
                                     />
                                 </div>
-                                <div className="p-4">
-                                    <p className="text-white text-base">
-                                    &quot;Top-notch craftsmanship and outstanding customer service. My home feels transformed thanks to their expert installation.&quot;
+                                <div className="pb-4">
+                                    <p className="text-white text-base text-center px-4">
+                                        &quot;Top-notch craftsmanship and outstanding customer service. My home feels transformed thanks to their expert installation.&quot;
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flex flex-col py-4">
+                    {/* <div className="flex flex-col py-4">
                         <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4 font-extrabold text-center text-gray-900">
                             Our Beliefs
                         </h2>
@@ -108,44 +108,74 @@ export default function About() {
                                 className=" w-full lg:w-1/2 py-4 rounded-4xl"
                             />
                         </div>
+                    </div> */}
+
+                    <div className="flex flex-col my-8">
+                        <h2 className="font-sans m-6 text-4xl sm:text-6xl py-4 mb-12 font-extrabold text-center text-gray-900">
+                            Our Beliefs
+                        </h2>
+                        <div className="flex flex-col md:flex-row items-center  shadow-xl rounded-xl outline-1 outline-gray-300 bg-stone-100 ">
+                            <div className="w-full lg:w-1/2">
+                                <Image
+                                    src="/images/services3-image.avif"
+                                    height={400}
+                                    width={1920}
+                                    alt="main page image"
+                                    priority
+                                    className="w-full rounded-t-xl md:rounded-t-none md:rounded-l-xl"
+                                />
+                            </div>
+                            <div className="w-full lg:w-1/2 flex flex-col text-center font-medium space-y-4 py-4">
+                                <p className=" text-2xl p-4 lg:text-4xl lg:px-16 text-gray-900">
+                                    With 20 years of experience in the trade, we guarantee an outstanding Design/Sale to project managed installation experience.
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex flex-col py-4">
-                        <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4  font-extrabold text-center text-gray-900">
+                    <div className="flex flex-col my-8">
+                        <h2 className="font-sans m-6 text-4xl sm:text-6xl py-4 mb-12 font-extrabold text-center text-gray-900">
                             Efficient installations
                         </h2>
-                        <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-2">
-                            <p className="text-gray-900 text-xl lg:text-4xl lg:w-1/2 p-2 py-4  ">
-                                Our comprehensive range of products is designed to meet the needs of both residential and trade customers.
-                            </p>
-                            <Image
-                                src="/images/about-us-page-image.jpeg"
-                                height={400}
-                                width={1920}
-                                alt="main page image"
-                                priority
-                                className=" w-full md:w-1/2 py-4 rounded-4xl "
-                            />
+                        <div className="flex flex-col md:flex-row items-center  shadow-xl rounded-xl outline-1 outline-gray-300 bg-stone-100 ">
+                            <div className="w-full lg:w-1/2">
+                                <Image
+                                    src="/images/about-us-page-image.jpeg"
+                                    height={400}
+                                    width={1920}
+                                    alt="main page image"
+                                    priority
+                                    className="w-full rounded-t-xl md:rounded-t-none md:rounded-l-xl"
+                                />
+                            </div>
+                            <div className="w-full lg:w-1/2 flex flex-col text-center font-medium space-y-4 py-4">
+                                <p className=" text-2xl p-4 lg:text-4xl lg:px-16 text-gray-900">
+                                    Our comprehensive range of products is designed to meet the needs of both residential and trade customers.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex flex-col py-4">
-                        <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4  font-extrabold text-center text-gray-900">
+                    <div className="flex flex-col my-8">
+                        <h2 className="font-sans m-6 text-4xl sm:text-6xl py-4 mb-12 font-extrabold text-center text-gray-900">
                             Areas Covered
                         </h2>
-                        <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-2">
-                            <p className="text-gray-900 text-xl lg:text-4xl lg:w-1/2 p-2 py-4  ">
-                                Our services are available in Leighton Buzzard, Milton Keynes, Dunstable, as well as the wider areas of Hertfordshire, Bedfordshire, Buckinghamshire, and Northants.
-                            </p>
-                            <Image
-                                src="/images/area-map.jpg"
-                                height={400}
-                                width={1920}
-                                alt="main page image"
-                                priority
-                                className=" w-full md:w-1/2 py-4 rounded-4xl"
-                            />
+                        <div className="flex flex-col md:flex-row items-center  shadow-xl rounded-xl outline-1 outline-gray-300 bg-stone-100 ">
+                            <div className="w-full lg:w-1/2">
+                                <Image
+                                    src="/images/area-map.jpg"
+                                    height={400}
+                                    width={1920}
+                                    alt="main page image"
+                                    priority
+                                    className="w-full rounded-t-xl md:rounded-t-none md:rounded-l-xl"
+                                />
+                            </div>
+                            <div className="w-full lg:w-1/2 flex flex-col text-center font-medium space-y-4 py-4">
+                                <p className=" text-2xl p-4 lg:text-4xl lg:px-16 text-gray-900">
+                                    Our services are available in Leighton Buzzard, Milton Keynes, Dunstable, as well as the wider areas of Hertfordshire, Bedfordshire, Buckinghamshire, and Northants.
+                                </p>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
             <ConsultationButton />
