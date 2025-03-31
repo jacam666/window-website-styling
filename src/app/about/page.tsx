f5cf3cc5be58cf86ab7ef23a54bfbcb872a856d7
jacam666 <ja6cam@gmail.com>
2025-03-31 18:13:03 +0100

    added card to about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,27 +9,85 @@ export default function About() {
             <Navbar />
             <div className="flex-1  m-2  rounded-lg">
                 <h2 className="font-sans text-5xl sm:text-8xl font-extrabold text-center text-gray-900 py-4 ">About Us</h2>
-                <div className="space-y-8 md:p-8  text-black text-center lg:text-xl mx-4 my-4">
-
+                <div className="space-y-8 md:p-8   mx-4 my-4">
                     <div className="flex flex-col w-full  lg:my-8 items-center px-4">
                         <h2 className="font-sans w-full  p-2 text-center md:p-6 text-xl lg:text-5xl mb-4  mx-2 text-gray-900">
                             Welcome to Sightline Windows, your trusted supplier and installer of high-quality UPVC and aluminium windows, French doors, UPVC doors, and patio doors.
                         </h2>
-                        <div className="bg-sky-900 w-full lg:w-1/2 rounded-4xl p-4">
+
+                    </div>
+                    {/* <div className="bg-sky-900 w-full  rounded-4xl p-4 ">
+                        <div className="flex flex-col w-full  lg:mt-6 items-center px-4">
                             <h2 className="font-sans p-2 text-4xl sm:text-6xl  font-extrabold text-center text-white">
                                 Trusted By Many
                             </h2>
-                            <p className="text-center my-4 md:p-6 text-xl lg:text-2xl mx-2 text-white">
+                            <p className="text-center md:p-6 text-xl lg:text-2xl mx-2 text-white">
                                 We are proud to be trusted by numerous clients for our professional window installation services.
                             </p>
-                            <Image
-                                src="/images/star-ratings (1).png"
-                                height={400}
-                                width={1920}
-                                alt="main page image"
-                                priority
-                                className="w-full"
-                            />
+                        </div>
+                        <Image
+                            src="/images/star-ratings (1).png"
+                            height={400}
+                            width={1020}
+                            alt="main page image"
+                            priority
+                            className="w-full"
+                        />
+                    </div> */}
+
+                    <div className="w-full bg-white font-bold rounded-lg shadow-lg p-4 outline-1 ">
+                        <div className="grid sm:grid-cols-3 gap-4">
+                            {/* Card 1 */}
+                            <div className="bg-sky-900 rounded-lg shadow-md overflow-hidden">
+                                <h3 className="flex font-bold text-lg justify-center pt-8">Customers name</h3>
+                                <div className="relative h-36">
+                                    <Image
+                                        src="/images/star-ratings (1).png"
+                                        alt="Sunset Over the Sea"
+                                        fill
+                                        className=""
+                                    />
+                                </div>
+                                <div className="p-4">
+                                    <p className="text-white text-base">
+                                    "Absolutely amazing service and quality. My new windows look fantastic, and the team was professional every step of the way."
+                                    </p>
+                                </div>
+                            </div>
+                            {/* Card 2 */}
+                            <div className="bg-sky-900 rounded-lg shadow-md overflow-hidden">
+                            <h3 className="flex font-bold text-lg justify-center pt-8">Customers name</h3>
+                                <div className="relative h-36">
+                                    <Image
+                                        src="/images/star-ratings (1).png"
+                                        alt="Sunset Over the Sea"
+                                        fill
+                                        className=""
+                                    />
+                                </div>
+                                <div className="p-4">
+                                    <p className="text-white text-base">
+                                    "Efficient and friendly! The installation was prompt, and the trade pricing made it a no-brainer. Highly recommend Sightline Windows."
+                                    </p>
+                                </div>
+                            </div>
+                            {/* Card 3 */}
+                            <div className="bg-sky-900 rounded-lg shadow-md overflow-hidden">
+                            <h3 className="flex font-bold text-lg justify-center pt-8">Customers name</h3>
+                                <div className="relative h-36">
+                                    <Image
+                                        src="/images/star-ratings (1).png"
+                                        alt="Sunset Over the Sea"
+                                        fill
+                                        className=""
+                                    />
+                                </div>
+                                <div className="p-4">
+                                    <p className="text-white text-base">
+                                    "Top-notch craftsmanship and outstanding customer service. My home feels transformed thanks to their expert installation."
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -37,8 +95,8 @@ export default function About() {
                         <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4 font-extrabold text-center text-gray-900">
                             Our Beliefs
                         </h2>
-                        <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-4">
-                            <p className="text-gray-900 text-xl lg:text-5xl lg:w-1/2 p-2 py-4  ">
+                        <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-2">
+                            <p className="text-gray-900 text-xl lg:text-4xl lg:w-1/2 p-2 py-4  ">
                                 With 20 years of experience in the trade, we guarantee an outstanding Design/Sale to project managed installation experience.
                             </p>
                             <Image
@@ -52,21 +110,20 @@ export default function About() {
                         </div>
                     </div>
                     <div className="flex flex-col py-4">
-                    <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4  font-extrabold text-center text-gray-900">
-                                Efficient installations
-                            </h2>
-                        <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-4">
-                            
-                            <p className="text-gray-900 text-xl lg:text-5xl lg:w-1/2 p-2 py-4  ">
+                        <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4  font-extrabold text-center text-gray-900">
+                            Efficient installations
+                        </h2>
+                        <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-2">
+                            <p className="text-gray-900 text-xl lg:text-4xl lg:w-1/2 p-2 py-4  ">
                                 Our comprehensive range of products is designed to meet the needs of both residential and trade customers.
                             </p>
                             <Image
-                                src="/images/about-us2.webp"
+                                src="/images/about-us-page-image.jpeg"
                                 height={400}
                                 width={1920}
                                 alt="main page image"
                                 priority
-                                className=" w-full lg:w-1/2 py-4 rounded-4xl"
+                                className=" w-full md:w-1/2 py-4 rounded-4xl "
                             />
                         </div>
                     </div>
@@ -75,17 +132,17 @@ export default function About() {
                             Areas Covered
                         </h2>
                         <div className="flex flex-col w-full md:flex-row lg:my-8 items-center px-2">
-                        <p className="text-gray-900 text-xl lg:text-4xl lg:w-1/2 p-2 py-4 ">
-                            Our services are available in Leighton Buzzard, Milton Keynes, Dunstable, as well as the wider areas of Hertfordshire, Bedfordshire, Buckinghamshire, and Northants. At Sightline Windows, we take pride in delivering exceptional customer service and top-quality products to meet all your window and door needs.
-                        </p>
-                        <Image
-                            src="/images/area-map.jpg"
-                            height={400}
-                            width={1920}
-                            alt="main page image"
-                            priority
-                            className=" w-full lg:w-1/2 py-4 rounded-4xl"
-                        />
+                            <p className="text-gray-900 text-xl lg:text-4xl lg:w-1/2 p-2 py-4  ">
+                                Our services are available in Leighton Buzzard, Milton Keynes, Dunstable, as well as the wider areas of Hertfordshire, Bedfordshire, Buckinghamshire, and Northants.
+                            </p>
+                            <Image
+                                src="/images/area-map.jpg"
+                                height={400}
+                                width={1920}
+                                alt="main page image"
+                                priority
+                                className=" w-full md:w-1/2 py-4 rounded-4xl"
+                            />
                         </div>
                     </div>
 
