630b85579eb22f06f09d64daf254803a6d66da2e
jacam666 <ja6cam@gmail.com>
2025-04-02 17:50:15 +0100

    adjusted homepage images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,50 +20,7 @@ export default function Home() {
           </div>
 
 
-          {/* <div className="flex flex-col lg:flex-row lg:my-8 items-center px-4 ">
-            <div className="flex flex-col w-full  lg:my-8 items-center px-4 lg:px-0 lg:outline-1 lg:gap-2 lg:outline-gray-300 lg:bg-stone-100 rounded-xl  ">
-              <Image
-                src="/images/services2-image.avif"
-                height={400}
-                width={1920}
-                alt="main page image"
-                priority
-                className="rounded-l-xl lg:rounded-t-xl w-full "
-              />
-              <div className="flex flex-col w-full  ">
-                <div className="flex flex-col py-4 ">
-                  <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4  font-extrabold text-center text-gray-900">
-                    Quality Materials
-                  </h2>
-                  <p className="font-sans p-2 text-center md:p-6 text-xl lg:text-4xl text-gray-900 py-4 mx-4">
-                    Sightline Windows, supplier and installer of high-quality UPVC and aluminium windows, French doors, UPVC doors, and patio doors.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col w-full  items-center px-4 lg:px-0 lg:outline-1 lg:outline-gray-300 lg:bg-stone-100 rounded-xl ">
-              <Image
-                src="/images/pexels-photo-5691503.webp"
-                height={400}
-                width={1920}
-                alt="main page image"
-                priority
-                className="rounded-r-xl  "
-              />
-              <div className="flex flex-col ">
-                <div className="flex flex-col py-4">
-                  <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4  font-extrabold text-center text-gray-900">
-                    Professional Installation
-                  </h2>
-                  <p className="font-sans p-2 text-center md:p-6 text-xl lg:text-4xl mx-2 text-gray-900">
-                    With 20 years of experience in the trade, we guarantee an outstanding Design/Sale to project managed installation experience.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div> */}
-          <div className="flex flex-col md:flex-row lg:my-8 items-center px-4 ">
-            {/* Card 1 */}
+          {/* <div className="flex flex-col md:flex-row lg:my-8 items-center px-4 ">
             <div className="flex flex-col w-full  lg:w-5/6 lg:mr-2 self-start px-4 lg:px-0 lg:outline-1 lg:gap-2 lg:outline-gray-300 lg:bg-stone-100 rounded-xl">
             
               <Image
@@ -85,8 +42,6 @@ export default function Home() {
                 </div>
               </div>
             </div>
-
-            {/* Card 2 */}
             <div className="flex flex-col w-full lg:w-5/6 lg:ml-2 self-start px-4 lg:px-0 lg:outline-1 lg:gap-2 lg:outline-gray-300 lg:bg-stone-100 rounded-xl">
               <Image
                 src="/images/services3-image.avif"
@@ -107,8 +62,55 @@ export default function Home() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
 
+          <div className="flex flex-col md:flex-row lg:my-8 items-center px-4">
+            {/* Card 1 */}
+            <div className="flex flex-col w-full lg:w-5/6 lg:mr-2 self-start px-4 lg:px-0 lg:outline-1 lg:gap-2 lg:outline-gray-300 lg:bg-stone-100 rounded-xl">
+              <div className="relative h-[400px] w-full">
+                <Image
+                  src="/images/services2-image.avif"
+                  alt="Quality Materials"
+                  fill
+                  className="object-cover rounded-l-xl lg:rounded-t-xl lg:rounded-l-none lg:rounded-tl-xl"
+                  priority
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="flex flex-col py-4">
+                  <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4 font-extrabold text-center text-gray-900">
+                    Quality Materials
+                  </h2>
+                  <p className="font-sans p-2 text-center md:p-6 text-xl lg:text-4xl text-gray-900 py-4 mx-4">
+                    Sightline Windows, supplier and installer of high-quality UPVC and aluminium windows, French doors, UPVC doors, and patio doors.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Card 2 */}
+            <div className="flex flex-col w-full lg:w-5/6 lg:ml-2 self-start px-4 lg:px-0 lg:outline-1 lg:gap-2 lg:outline-gray-300 lg:bg-stone-100 rounded-xl">
+              <div className="relative h-[400px] w-full">
+                <Image
+                  src="/images/services3-image.avif"
+                  alt="Professional Installation"
+                  fill
+                  className="object-cover rounded-r-xl lg:rounded-t-xl lg:rounded-tr-xl lg:rounded-br-none"
+                  priority
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="flex flex-col py-4">
+                  <h2 className="font-sans p-2 text-4xl sm:text-6xl py-4 font-extrabold text-center text-gray-900">
+                    Professional Installation
+                  </h2>
+                  <p className="font-sans p-2 text-center md:p-6 text-xl lg:text-4xl text-gray-900 py-4 mx-4 lg:mx-2">
+                    With 20 years of experience in the trade, we guarantee an outstanding Design/Sale to project managed installation experience.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
 
 
